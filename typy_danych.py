171df4dbb004a2aeb7ee17bb100da5e97449fd4f
dmt-herma-pawel <dmt.pawel@lo5.bielsko.pl>
2023-07-19 09:56:27 +0200

    Create typy_danych.py

---
+++ typy_danych.py
@@ -0,0 +1,53 @@
+# deklaracja zmiennej
+slowo = "Dzień dobry" # typ str - słowo
+
+# nadpisanie zmiennej ze zmianą jej typu danych
+slowo = 3 # typ int - liczba całkowita
+
+# wydrukowanie informacji do terminala - polecenie print
+print(slowo)
+
+slowo = "Cześć"
+
+liczba_calkowita = 3
+
+liczba_zmiennoprzecinkowa = 3.5 # typ float
+
+logiczna = True # typ bool - przyjmuje wartość True / False
+
+pojedynczy_znak = 'a' # typ char - character - przechowuje pojedynczy znak
+
+lista_liczb = [1,2,3,4]
+
+#indexy listy   0      1      2
+lista_slow = ["ala","bela","cela"] # listy mogą przechowywać dowolny typ danych
+
+lista_randomowa = [ 1, "Arbuz", False ]
+
+# 2 sposoby na wypisywanie elementów tablicy
+
+# sposób 1 - wypisywanie całej tablicy
+print(lista_liczb)
+
+# sposób 2 - wypisywanie konkretnego elementu tablicy
+print(lista_slow[1]) # podajemy index elementu listy
+
+# UWAGA! W programowaniu odliczamy od 0
+
+# sprawdzenie typu danych zmiennej
+
+print( type(slowo) )
+
+print( type(liczba_calkowita) )
+
+print( type(liczba_zmiennoprzecinkowa) )
+
+print( type(logiczna) )
+
+print( type(lista_slow) )
+
+print( type(lista_slow[0]) )
+
+print( type(lista_liczb) )
+
+print( type(lista_liczb[0]) )
